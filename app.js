788cffff3bd8a299c2e9acb6249df8bebbc62127
Bhumi Shah <shah.bhu@northeastern.edu>
2023-11-20 22:48:40 -0500

    first commit

---
+++ app.js
@@ -0,0 +1,15 @@
+//const express = require("express");
+ import express from "express";
+ import Lab5 from "./lab5.js";
+import cors from "cors";
+import CourseRoutes from "./courses/routes.js";
+import ModuleRoutes from "./modules/routes.js";
+import cors from "cors";
+import "dotenv/config";
+const app = express();
+app.use(cors());
+CourseRoutes(app);
+app.use(express.json());
+ModuleRoutes(app);
+Lab5(app);
+app.listen(process.env.PORT || 4000);
