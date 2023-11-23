40e0e7e42dc1ce447140b0d9694751b622714a33
Bhumi Shah <shah.bhu@northeastern.edu>
2023-11-22 23:43:42 -0500

    fdcd

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 //const express = require("express");
  import express from "express";
  import Lab5 from "./lab5.js";
+ import HelloRoutes from "./hello.js";
 import cors from "cors";
 import CourseRoutes from "./courses/routes.js";
 import ModuleRoutes from "./modules/routes.js";
